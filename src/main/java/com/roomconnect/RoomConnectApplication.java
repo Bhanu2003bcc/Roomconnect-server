8cf518c46c9350b2aa596d9b0a7a53315ac151d7
Mr. Er <bhanupratapsingh2939522@gmail.com>
2026-07-24 12:05:01 +0530

    Fix compilation error by setting spring.data.redis.repositories.enabled=false in application-prod.yml

--- src/main/java/com/roomconnect/RoomConnectApplication.java
+++ src/main/java/com/roomconnect/RoomConnectApplication.java
@@ -2,12 +2,10 @@ package com.roomconnect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableRedisRepositories(enabled = false)
 public class RoomConnectApplication {
     public static void main(String[] args) {
         SpringApplication.run(RoomConnectApplication.class, args);
